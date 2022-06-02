1f5c54a1eb13ef6291ce83b3356a747075a6e9ac
Zeno Driesen <zeno.driesen@gmail.com>
2022-06-02 10:58:32 +0200

    circle expands
    circle expands but goes outside of the container

--- js/script.js
+++ js/script.js
@@ -4,17 +4,29 @@ function addCircle (event) {
 
     newDiv.className = "circle";
     // Styling
-    newDiv.style.width = "20px";
-    newDiv.style.height = "20px";
+    newDiv.style.width = "0";
+    newDiv.style.height = "0";
     newDiv.style.border = "2px solid #000";
     newDiv.style.borderRadius = "50%";
     newDiv.style.alignItems = "center";
     newDiv.style.position = "absolute";
     newDiv.style.left = event.pageX + "px";
     newDiv.style.top = event.pageY + "px";
+
     //add the newly created element and its content to the DOM
     const currentDiv = document.querySelector('.challengeContainer');
     currentDiv.appendChild(newDiv);
+
+    function growCircle (radius) {
+        newDiv.style.transitionProperty = "width, height";
+        newDiv.style.transitionDuration = "4s";
+        newDiv.style.transform = "translateX(-50%) translateY(-50%)";
+        setTimeout(() => {
+            newDiv.style.width = radius * 2 + "px";
+            newDiv.style.height = radius * 2 + "px";
+        }, 0);
+    }
+    growCircle(200);
 }
 
 // CHALLENGE CONTAINER
