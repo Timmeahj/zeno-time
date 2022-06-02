829adfe242158963fbb482ebc837eef6d73e8395
Zeno Driesen <zeno.driesen@gmail.com>
2022-06-02 10:17:18 +0200

    position circle
    The circle is now being created at the location where the cursor is. It only creates a circle inside the container

--- js/script.js
+++ js/script.js
@@ -1,18 +1,23 @@
-function addCircle () {
-    //create new div
+function addCircle (event) {
+    // create new div
     const newDiv = document.createElement("div");
 
-    //let's first give it some content
-    const content = document.createTextNode("Hi I will become a circle!");
-
-    //add the text node to the new div
-    newDiv.appendChild(content);
-
+    newDiv.className = "circle";
+    // Styling
+    newDiv.style.width = "20px";
+    newDiv.style.height = "20px";
+    newDiv.style.border = "2px solid #000";
+    newDiv.style.borderRadius = "50%";
+    newDiv.style.alignItems = "center";
+    newDiv.style.position = "absolute";
+    newDiv.style.left = event.pageX + "px";
+    newDiv.style.top = event.pageY + "px";
     //add the newly created element and its content to the DOM
     const currentDiv = document.querySelector('.challengeContainer');
     currentDiv.appendChild(newDiv);
 }
 
+// CHALLENGE CONTAINER
 const challengeContainer = document.querySelector('.challengeContainer');
 
 challengeContainer.addEventListener('click', addCircle);
