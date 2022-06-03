8a0d5ef31228c27f3623ea34d0c509bb1d9e5c1a
Zeno Driesen <Zeno@iMac-van-Zeno.local>
2022-06-03 11:06:39 +0200

    color, animal, sport
    changes for the fav color, fav animal and fav sport, used mouse click or hover, also added a button you can click

--- js/script.js
+++ js/script.js
@@ -7,6 +7,7 @@ quote[0].addEventListener('mouseover', function () {
     document.body.style.fontFamily = "Roboto, sans-serif";
     document.body.style.fontWeight = "800";
     document.body.style.fontStyle = "italic";
+    quote[0].innerText = `I do love Impact, but also Roboto`;
 })
 
 //Center the header
@@ -15,11 +16,56 @@ header[0].style.display = "flex";
 header[0].style.flexDirection = "column";
 header[0].style.alignItems = "center";
 
-let headerRow = document.querySelector('.fullHeight');
-headerRow.style.width = "100%";
+    //header row content styling, the image with the words
+    let headerRow = document.querySelector('.fullHeight');
+    headerRow.style.width = "100%";
 
+    let iDontLikeFlex = document.querySelector('.iDontLikeFlex');
+    iDontLikeFlex.style.justifyContent = "center";
+    iDontLikeFlex.style.width = "30%";
 
+    let zeno = document.querySelector('.cedric');
+    zeno.style.width = "70%";
+    let imageZeno = document.querySelector('.backgroundImage');
+    imageZeno.style.backgroundSize = "auto 60%";
 
+//Change favourite color
+let favColor = document.querySelector('.bordeaux');
+let colorDescription = document.getElementById('colourDescription');
+favColor.addEventListener('click', function (){
+    favColor.style.background = "rgb(0 217 255)";
+    document.body.style.color = "rgb(200 200 200)";
+    colorDescription.innerText = `RA RA RA............. AND IT'S BLUE`;
+})
+
+//Change favourite animal
+let animalDescription = document.getElementById('animalDescription');
+let animalButton = document.createElement("button");
+let animalPicture = document.querySelector('.animalPic');
+animalButton.innerHTML = "No really?!";
+animalButton.addEventListener('click', function () {
+    animalDescription.innerText = "I do like mythical beings, and it would be so cool if bigfoot or a yeti would be real. But then again poor them, they would be stuck in a lab or zoo for life. But my all time favourite animal is ManBearPig."
+    animalPicture.style.background = "url(img/manBearPig.jpg)";
+    animalPicture.style.backgroundRepeat = "no-repeat";
+    animalPicture.style.backgroundPosition = "center";
+    animalPicture.style.backgroundSize = "auto 100%";
+})
+animalDescription.appendChild(animalButton);
+
+//Change favourite sport
+let sportDescription = document.getElementById('sportDescription');
+let sportPicture = document.querySelector('.hockey');
+sportPicture.addEventListener('mouseover', function (){
+    sportDescription.innerText = "Luckily I'm already a Magic Mike for my girlfriend, but nicely done Channing you made it from the stripper pole life to stardom. So respect for those Mikes out there, keep on swinging. I would rather say that my favourite sport is cheese rolling. It's just all laughs and cheese."
+    sportPicture.style.background = "url(img/cheese.gif)";
+    sportPicture.style.backgroundRepeat = "no-repeat";
+    sportPicture.style.backgroundPosition = "center";
+    sportPicture.style.backgroundSize = "auto 100%";
+})
+
+//Change favourite character
+let charDescription = document.getElementById('characterDescription');
+let charPicture = document.querySelector('.khal');
 
 // Handler 1
 function createGrowingCircle (event) {
